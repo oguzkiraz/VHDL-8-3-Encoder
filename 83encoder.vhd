30371ebe5e2a053b217525de55b6e88c7dff6c1a
oguzkiraz <48150244+oguzkiraz@users.noreply.github.com>
2021-08-18 02:23:14 +0300

    Update 83encoder.vhd

--- 83encoder.vhd
+++ 83encoder.vhd
@@ -1,18 +1,18 @@
 Library Ieee;
 Use Ieee.std_logic_1164.all;
 
-Entity Eencoder83 is				--Entity ac
+Entity Eencoder83 is				--Entity
 
-  Port(input : in std_logic_vector (7 downto 0); 	-- 8 bit giris oldugu icin 7 downto 0
-  output : out std_logic_vector (2 downto 0);	-- 3 cikis
+  Port(input : in std_logic_vector (7 downto 0); 	-- 7 downto 0 for 8 bit input
+  output : out std_logic_vector (2 downto 0);	-- 3 bit output
 
-end Eencoder83;					--Entitiy kapa
+end Eencoder83;					--Entitiy close
 
-Architecture Behv of Eencoder83 is 		-- Entity davranislarini yazma
+Architecture Behv of Eencoder83 is 		-- Entity behaviour
 
   Begin
 
-  output <= "111" when input(7) = '1' else		--7. giris 1 geldiginde output a 111 ata
+  output <= "111" when input(7) = '1' else
   "110" when input(6) = '1' else
   "101" when input(5) = '1' else
   "100" when input(4) = '1' else
